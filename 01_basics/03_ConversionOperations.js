dd94a9a21f39d24eb3360df38ab7d0d295ed0b05
AJ221 <ajuneja723@gmail.com>
2025-11-23 14:19:42 +0530

    done changes

--- 01_basics/03_ConversionOperations.js
+++ 01_basics/03_ConversionOperations.js
@@ -3,5 +3,11 @@ let score="98";
 console.log(score)
 console.log(typeof(score))
 
-let numberScore=Number(score)
-console.log(typeof numberScore)
+console.log(typeof Number(score))
+
+let numberScore="123abc"
+let convertedScore=Number(numberScore)
+console.log(typeof(convertedScore)) //123
+console.log(Number(numberScore)) //NaN
+
+console.log(parseInt(numberScore)) //123
