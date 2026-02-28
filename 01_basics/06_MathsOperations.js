15afd08765ae541d3cfc88cea844fcdaa9cea6c3
AJ221 <ajuneja723@gmail.com>
2026-02-28 12:50:40 +0530

    dcsdc

--- 01_basics/06_MathsOperations.js
+++
@@ -1,4 +0,0 @@
-let lLimit=10;
-let uLimit=20;
-
-console.log(Math.round(Math.random()*(uLimit-lLimit))+lLimit); //0 to 1
--- 07_DateTime.js
+++
@@ -1,3 +0,0 @@
-let currentDate=new Date();
-console.log(Date.now().toPrecision(15));
-// console.log(Date.now());
